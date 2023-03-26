fc3b596b689382d38a48cd81bec4ec7493eb3d35
Siarhei Prakapovich <prokser27@icloud.com>
2023-03-26 23:45:38 +0300

    fix: rewrite refs

--- rss-react-components/src/pages/form.tsx
+++ rss-react-components/src/pages/form.tsx
@@ -5,6 +5,7 @@ import "../styles/form-styles.css"
 
 export default class Form extends React.Component<FormProps, FormState> {
   nameInput: React.RefObject<HTMLInputElement>;
+  surnameInput: React.RefObject<HTMLInputElement>;
   dateInput: React.RefObject<HTMLInputElement>;
   selectionInput: React.RefObject<HTMLInputElement>;
   checkboxInput: React.RefObject<HTMLInputElement>;
@@ -34,37 +35,32 @@ export default class Form extends React.Component<FormProps, FormState> {
       submitted: false,
     };
     this.nameInput = React.createRef();
+    this.surnameInput = React.createRef();
     this.dateInput = React.createRef();
     this.selectionInput = React.createRef();
     this.checkboxInput = React.createRef();
     this.switcherInput = React.createRef();
     this.fileInput = React.createRef();
 
-    this.handleFormInputChange = this.handleFormInputChange.bind(this)
+    this.handleFormInputChange = this.handleFormInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     
   }
 
   handleFormInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      
+    
+  };
+
+  handleSubmit = () => {
+
   }
+
   render() {
     return (
       <div className="form-container">
         <form className="form-content">
           <div className="form-row">
             <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="surname"
-              name="surname"
-              ref={this.nameInput}
-              value={this.state.surname}
-              onChange={this.handleFormInputChange} />
-            {this.state.errors.surname && <span>{this.state.errors.surname}</span>}
-          </div>
-          <div className="form-row">
-            
-            <label htmlFor="surname">Surname:</label>
             <input
               type="text"
               id="name"
@@ -74,6 +70,20 @@ export default class Form extends React.Component<FormProps, FormState> {
               onChange={this.handleFormInputChange} />
             {this.state.errors.name && <span>{this.state.errors.name}</span>}
           </div>
+          <div className="form-row">
+            <label htmlFor="surname">Surname:</label>
+            <input
+              type="text"
+              id="surname"
+              name="surname"
+              ref={this.surnameInput}
+              value={this.state.surname}
+              onChange={this.handleFormInputChange} />
+            {this.state.errors.surname && <span>{this.state.errors.surname}</span>}
+          </div>
+          <button type="submit">
+            Submit
+          </button>
         </form>
       </div>
     )
