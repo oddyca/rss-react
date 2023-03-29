c9316e2f8b12535ec369b33877963b54003f504a
Siarhei Prakapovich <prokser27@icloud.com>
2023-03-29 19:20:43 +0300

    refactor: add card decoys to main page

--- rss-react-components/src/pages/home.tsx
+++ rss-react-components/src/pages/home.tsx
@@ -12,7 +12,26 @@ export default class Home extends Component<object, HomeState> {
     return (
       <div className="home-container">
         <Search />
-        <h3>CARDS..</h3>
+        <div className="cards-container">
+          <div className="cards">
+            <div className="card-title">
+              <h3>Card #1</h3>
+            </div>
+            <div className="card-body">
+              <p>Card info</p>
+              <p>Lorem Ipsum</p>
+            </div>
+          </div>
+          <div className="cards">
+            <div className="card-title">
+              <h3>Card #2</h3>
+            </div>
+            <div className="card-body">
+              <p>Card info</p>
+              <p>Lorem Ipsum</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
