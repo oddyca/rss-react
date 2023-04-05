b170ef16a45da591a162df5aaf5ebd9d1c402705
Siarhei Prakapovich <prokser27@icloud.com>
2023-04-05 15:17:27 +0300

    refactor: redesign form

--- rss-react-components/src/pages/form.tsx
+++ rss-react-components/src/pages/form.tsx
@@ -60,100 +60,158 @@ export default function Form() {
 
   return (
     <div className="form-container">
-      <form className="form-content" onSubmit={handleSubmit(handleFormSubmit)}>
-        <div className="form-row">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            {...register('name', {
-              required: 'Name is required',
-              pattern: {
-                value: /^[A-Z][a-z]*$/,
-                message: 'First name must contain only letters and start with an uppercased letter',
-              },
-            })}
-          />
-          {errors.name && <span className="submit-error">{errors.name.message}</span>}
+      <div className="form-decoration">
+        <div className="errors-container">
+          <div>{errors.name && <span className="submit-error">{errors.name.message}</span>}</div>
+          <div>
+            {errors.surname && <span className="submit-error">{errors.surname.message}</span>}
+          </div>
+          <div>{errors.date && <span className="submit-error">{errors.date.message}</span>}</div>
+          <div></div>
+          <div>
+            {errors.checkbox && <span className="submit-error">{errors.checkbox.message}</span>}
+          </div>
+          <div>
+            {errors.gender && <span className="submit-error">{errors.gender.message}</span>}
+          </div>
+          <div>{errors.pfp && <span className="submit-error">{errors.pfp.message}</span>}</div>
         </div>
-        <div className="form-row">
-          <label htmlFor="surname">Surname:</label>
-          <input
-            type="text"
-            id="surname"
-            {...register('surname', {
-              required: 'Last name is required',
-              pattern: {
-                value: /^[A-Z][a-z]*$/,
-                message: 'Last name must contain only letters and start with an uppercased letter',
-              },
-            })}
-          />
-          {errors.surname && <span className="submit-error">{errors.surname.message}</span>}
+        <div className="form-border">
+          <form className="form-content" onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className="form-dec-top-left">
+              <div className="dec-top-row1">FORM v0.431sd 05wwetf</div>
+              <div className="dec-top-row2">
+                <p>
+                  237320
+                  <br />
+                  929377
+                  <br />
+                  374320
+                </p>
+                <p>
+                  552030
+                  <br />
+                  859668
+                  <br />
+                  903345
+                </p>
+                <p>
+                  002030
+                  <br />
+                  929308
+                  <br />
+                  237302
+                </p>
+              </div>
+            </div>
+            <div className="form-row">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                className="input-field"
+                {...register('name', {
+                  required: 'Name is required',
+                  pattern: {
+                    value: /^[A-Z][a-z]*$/,
+                    message:
+                      'First name must contain only letters and start with an uppercased letter',
+                  },
+                })}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="surname">Surname:</label>
+              <input
+                type="text"
+                id="surname"
+                className="input-field"
+                {...register('surname', {
+                  required: 'Last name is required',
+                  pattern: {
+                    value: /^[A-Z][a-z]*$/,
+                    message:
+                      'Last name must contain only letters and start with an uppercased letter',
+                  },
+                })}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="date">Date:</label>
+              <input
+                className="input-field"
+                type="date"
+                id="date"
+                {...register('date', {
+                  required: 'Date is required',
+                })}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="dropdown">Select country of choice:</label>
+              <select
+                id="dropdown"
+                {...register('country', { onChange: handleFormInputChange })}
+                value={state.selection}
+              >
+                <option value="USA">USA</option>
+                <option value="United Kingdom">United Kingdom</option>
+                <option value="Canada">Canada</option>
+              </select>
+            </div>
+            <div className="form-row">
+              <label htmlFor="checkbox">I consent to the processing of my personal data</label>
+              <input
+                type="checkbox"
+                id="checkbox"
+                {...register('checkbox', {
+                  required: 'This field is required',
+                })}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="radio-m">Male</label>
+              <input
+                type="radio"
+                id="radio-m"
+                value="Male"
+                {...register('gender', {
+                  required: 'This field is required',
+                })}
+              />
+              <label htmlFor="radio-f">Female</label>
+              <input
+                type="radio"
+                id="radio-f"
+                value="Female"
+                {...register('gender', {
+                  required: 'This field is required',
+                })}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="pfp-upload">Upload your profile picture</label>
+              <input
+                type="file"
+                accept="image/*"
+                id="pfp-upload"
+                {...register('pfp', {
+                  required: 'Image is required',
+                })}
+              />
+            </div>
+            <button type="submit" className="submit-form-btn">
+              Submit
+            </button>
+          </form>
         </div>
-        <div className="form-row">
-          <label htmlFor="date">Date:</label>
-          <input
-            type="date"
-            id="date"
-            {...register('date', {
-              required: 'Date is required',
-            })}
-          />
-          {errors.date && <span className="submit-error">{errors.date.message}</span>}
+        <div className="form-bottom-decoration">
+          <p>
+            3.0xF.234{`[`}WRR{`]`}
+          </p>
+          <p>荒坂社</p>
         </div>
-        <div className="form-row">
-          <label htmlFor="dropdown">Select country of choice:</label>
-          <select
-            id="dropdown"
-            {...register('country', { onChange: handleFormInputChange })}
-            value={state.selection}
-          >
-            <option value="USA">USA</option>
-            <option value="United Kingdom">United Kingdom</option>
-            <option value="Canada">Canada</option>
-          </select>
-        </div>
-        <div className="form-row">
-          <label htmlFor="checkbox">I consent to the processing of my personal data</label>
-          <input
-            type="checkbox"
-            id="checkbox"
-            {...register('checkbox', {
-              required: 'This field is required',
-            })}
-          />
-          {errors.checkbox && <span className="submit-error">{errors.checkbox.message}</span>}
-        </div>
-        <div className="form-row">
-          <label htmlFor="radio-m">Male</label>
-          <input
-            type="radio"
-            id="radio-m"
-            value="Male"
-            {...register('gender', {
-              required: 'This field is required',
-            })}
-          />
-          <label htmlFor="radio-f">Female</label>
-          <input
-            type="radio"
-            id="radio-f"
-            value="Female"
-            {...register('gender', {
-              required: 'This field is required',
-            })}
-          />
-          {errors.gender && <span className="submit-error">{errors.gender.message}</span>}
-        </div>
-        <div className="form-row">
-          <label htmlFor="pfp-upload">Upload your profile picture</label>
-          <input type="file" accept="image/*" id="pfp-upload" {...register('pfp')} />
-        </div>
-        <button type="submit" className="submit-form-btn">
-          Submit
-        </button>
-      </form>
+      </div>
       <div className="form-cards">
         {state.cards?.length !== 0 ? (
           <Cards cards={state.cards} />
