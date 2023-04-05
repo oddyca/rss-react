7f4768a02069ca79340c1487caa1af43147fe8c8
Siarhei Prakapovich <prokser27@icloud.com>
2023-04-05 15:49:38 +0300

    refactor: redesign from cards

--- rss-react-components/src/components/cards.tsx
+++ rss-react-components/src/components/cards.tsx
@@ -7,33 +7,42 @@ const Cards = ({ cards }: CardsProps) => {
   const renderCards = () => {
     return cards?.map((card, id) => {
       return (
-        <div className="cards" key={id}>
-          <div className="card-title">
-            <h3>Card #{id + 1}</h3>
-          </div>
-          <div className="card-body">
-            <div className="card-pfp" style={{ backgroundImage: `url(${card.pfp})` }}></div>
-            <div className="card-body_col">
-              {Object.keys(card)
-                .slice(0, -1)
-                .map((key, id) => {
-                  return (
-                    <div className="card-body_row" key={id}>
-                      <p>{key}:</p>
-                    </div>
-                  );
-                })}
-            </div>
-            <div className="card-body_col">
-              {Object.values(card)
-                .slice(0, -1)
-                .map((value, id) => {
-                  return (
-                    <div className="card-body_row" key={id}>
-                      <p>{value}</p>
-                    </div>
-                  );
-                })}
+        <div className="cards-form" key={id}>
+          <div className="form-cards-border">
+            <div className="form-cards-container">
+              <div className="card-title">
+                <p className="card-title_decoration">
+                  /LOADED
+                  <br />
+                  v:43.234cd.xf
+                </p>
+                <h3>CARD_{id + 1}</h3>
+              </div>
+              <div className="card-form-body">
+                <div className="card-pfp" style={{ backgroundImage: `url(${card.pfp})` }}></div>
+                <div className="card-body_col">
+                  {Object.keys(card)
+                    .slice(0, -1)
+                    .map((key, id) => {
+                      return (
+                        <div className="card-body_row" key={id}>
+                          <p>{key}:</p>
+                        </div>
+                      );
+                    })}
+                </div>
+                <div className="card-body_col">
+                  {Object.values(card)
+                    .slice(0, -1)
+                    .map((value, id) => {
+                      return (
+                        <div className="card-body_row" key={id}>
+                          <p>{value}</p>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
             </div>
           </div>
         </div>
